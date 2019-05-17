e6344bc8e7e2cd25e6d4eff79235ac9ec3112b25
hubble-02 <cosmosaditya02@gmail.com>
2019-05-17 22:25:17 +0530

    C++ assignments

---
+++ c++_assignments/2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+#define min(x,y) (x>y)?y:x //macros for minimum of 2 nos
+
+int best_step( int a[], int i, int k, int n); //function for finding best step from i+1 to i+k
+
+int min_step( int a[], int n); //function that returns minimum number of steps
+
+int main() {
+
+    int n;
+    cout<<" Kindly enter the length of array ";
+    cin >> n;
+
+    int* a = new int[n]; //dynamic array of n
+    cout << "Now enter the array of steps I should climb ";
+    for ( size_t i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int s = min_step(a,n);
+
+
+    cout << endl << "Minimum number of steps I took was " << s << endl;
+    delete[] a;
+    return 0;
+}
+
+int best_step( int a[], int i, int t, int n) {
+
+    int m = 0, l;
+
+    l = min( t+i , n-1 );
+
+    for ( size_t j = i+1; j <= l; j++ ) {
+        if( m <= a[j] ) {
+            m = a[j];
+            i = j;
+        }
+    }
+    return i;
+}
+
+int min_step( int a[], int n) {
+
+    int i = 0, t = a[i] , s = 0;
+    while ( i < n-1 ) {
+
+        if ( i < n-1 && (t+i) < n) { //if we are less than last number we run the following
+
+            i = best_step(a,i,t,n);
+            t = a[i];
+            s++;
+
+        }
+        else {
+            s++;
+            break;
+        }
+
+        if( t == 0 ) {
+            cout << "I am stuck at 0 help me :(";
+            return 0;
+        }
+    }
+
+    return s;
+}
